fdf763d214c14520da813ff3be02fffb4a3f37c6
Makolicious <m.aleksandarr@gmail.com>
2017-10-11 00:26:12 +1100

    The real deal

--- game-of-threes/Program.cs
+++ game-of-threes/Program.cs
@@ -10,6 +10,31 @@ namespace game_of_threes
     {
         static void Main(string[] args)
         {
+            int subjectNum = 100;
+            Console.WriteLine(subjectNum);
+
+            while (subjectNum != 1)
+            {
+                if (subjectNum % 3 == 0)
+                {
+                    Console.WriteLine(subjectNum + " 0");
+                    subjectNum = subjectNum / 3;
+                }
+
+                else if ((subjectNum + 1) % 3 == 0)
+                {
+                    Console.WriteLine(subjectNum + " +1");
+                    subjectNum = (subjectNum + 1) / 3;
+                }
+
+                else if ((subjectNum - 1) % 3 == 0)
+                {
+                    Console.WriteLine(subjectNum + " -1");
+                    subjectNum = (subjectNum - 1) / 3;
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }

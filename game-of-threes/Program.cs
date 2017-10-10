85b8d15803b6aa778ca025cbc7fd79c5e182c110
Makolicious <m.aleksandarr@gmail.com>
2017-10-11 02:30:12 +1100

    Fixed and improved :)

--- game-of-threes/Program.cs
+++ game-of-threes/Program.cs
@@ -10,47 +10,45 @@ namespace game_of_threes
     {
         static void Main(string[] args)
         {
+        start:
             Console.WriteLine("Enter a number.");
             string userInput = Console.ReadLine();
             int subjectNum;
-            bool inputIsNum = false;
 
-            while (inputIsNum == false)
+            try
             {
-                if (int.TryParse(userInput, out subjectNum))
+                subjectNum = Int32.Parse(userInput);
+                Console.WriteLine(subjectNum);
+                while (subjectNum != 1)
                 {
-                    Console.WriteLine(subjectNum);
-                    while (subjectNum != 1)
+                    if (subjectNum % 3 == 0)
                     {
-                        if (subjectNum % 3 == 0)
-                        {
-                            Console.WriteLine(subjectNum + " 0");
-                            subjectNum = subjectNum / 3;
-                        }
-
-                        else if ((subjectNum + 1) % 3 == 0)
-                        {
-                            Console.WriteLine(subjectNum + " +1");
-                            subjectNum = (subjectNum + 1) / 3;
-                        }
-
-                        else if ((subjectNum - 1) % 3 == 0)
-                        {
-                            Console.WriteLine(subjectNum + " -1");
-                            subjectNum = (subjectNum - 1) / 3;
-                        }
+                        Console.WriteLine(subjectNum + " 0");
+                        subjectNum = subjectNum / 3;
                     }
-                    Console.WriteLine(subjectNum);
-                    Console.ReadLine();
-                    inputIsNum = true;
+
+                    else if ((subjectNum + 1) % 3 == 0)
+                    {
+                        Console.WriteLine(subjectNum + " +1");
+                        subjectNum = (subjectNum + 1) / 3;
+                    }
+
+                    else if ((subjectNum - 1) % 3 == 0)
+                    {
+                        Console.WriteLine(subjectNum + " -1");
+                        subjectNum = (subjectNum - 1) / 3;
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Please enter a valid number.");
-                    Console.ReadLine();
-                    
-                }
+                Console.WriteLine(subjectNum);
+                Console.WriteLine("Finished!");
+                goto start;
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a valid number.");
+                goto start;
             }
         }
     }
 }
+

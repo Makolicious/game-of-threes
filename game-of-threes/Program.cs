eba964642ca0a1193c31d05bfcb4638bca6d4b7d
Makolicious <m.aleksandarr@gmail.com>
2017-10-11 19:40:43 +1100

    Added an exit command

--- game-of-threes/Program.cs
+++ game-of-threes/Program.cs
@@ -11,37 +11,43 @@ namespace game_of_threes
         static void Main(string[] args)
         {
         start:
-            Console.WriteLine("Enter a number.");
+            Console.WriteLine("Enter a number to run the application, enter 'exit' to close the application.");
             string userInput = Console.ReadLine();
             int subjectNum;
 
             try
             {
-                subjectNum = Int32.Parse(userInput);
-                Console.WriteLine(subjectNum);
-                while (subjectNum != 1)
+                if (userInput == "exit")
                 {
-                    if (subjectNum % 3 == 0)
-                    {
-                        Console.WriteLine(subjectNum + " 0");
-                        subjectNum = subjectNum / 3;
-                    }
-
-                    else if ((subjectNum + 1) % 3 == 0)
-                    {
-                        Console.WriteLine(subjectNum + " +1");
-                        subjectNum = (subjectNum + 1) / 3;
-                    }
-
-                    else if ((subjectNum - 1) % 3 == 0)
-                    {
-                        Console.WriteLine(subjectNum + " -1");
-                        subjectNum = (subjectNum - 1) / 3;
-                    }
+                    Environment.Exit(0);
                 }
-                Console.WriteLine(subjectNum);
-                Console.WriteLine("Finished!");
-                goto start;
+                else
+                {
+                    subjectNum = Int32.Parse(userInput);
+                    while (subjectNum != 1)
+                    {
+                        if (subjectNum % 3 == 0)
+                        {
+                            Console.WriteLine(subjectNum + " 0");
+                            subjectNum = subjectNum / 3;
+                        }
+
+                        else if ((subjectNum + 1) % 3 == 0)
+                        {
+                            Console.WriteLine(subjectNum + " +1");
+                            subjectNum = (subjectNum + 1) / 3;
+                        }
+
+                        else if ((subjectNum - 1) % 3 == 0)
+                        {
+                            Console.WriteLine(subjectNum + " -1");
+                            subjectNum = (subjectNum - 1) / 3;
+                        }
+                    }
+                    Console.WriteLine(subjectNum);
+                    Console.WriteLine("Finished!");
+                    goto start;
+                }
             }
             catch
             {
